dfc9bdaacf80c220669d32b4d2d58861a1bb6c05
João Pedro Soares do Monte <82397148+JpSoares17@users.noreply.github.com>
2025-01-08 23:55:32 -0300

    Fix: cv.imshow

--- src/predict.py
+++ src/predict.py
@@ -1,10 +1,35 @@
 from ultralytics import YOLO
 import cv2
 
-def main():
-    model = YOLO("./models/carTimer.pt")
-    model.predict("./static/videos/parking_crop.mp4", show=True, stream=True)
+# Carregue o modelo treinado
+model = YOLO("./models/carTimer.pt")
 
+# Abra o vídeo
+video_path = "./static/videos/parking_1920_1080.mp4"
+cap = cv2.VideoCapture(video_path)
 
-if __name__ == "__main__":
-    main()
+# Configure o vídeo de saída (opcional)
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output_video.mp4', fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Faça a inferência
+    results = model(frame)
+
+    # Obtenha os quadros com as detecções desenhadas
+    annotated_frame = results[0].plot()
+
+    # Salve o quadro no vídeo de saída
+    out.write(annotated_frame)
+
+    # Pressione 'q' para sair
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
